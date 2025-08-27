3f9bb2f97e26ed9b51fb3ca2aef9fbf060747731
Sarang Shahane <sarangshahane321@gmail.com>
2025-08-27 23:29:43 +0530

    :hammer: Fetched the Courses in the upcoming events. Changed the name of the shortcode

--- classes/class-cpsc-shortcodes.php
+++ classes/class-cpsc-shortcodes.php
@@ -36,7 +36,7 @@ class Cpsc_Frontend {
 	 */
 	public function __construct() {
 
-		add_shortcode( 'workshops', array( $this, 'load_workshop_shortcode' ) );
+		add_shortcode( 'cpsc_upcoming_events', array( $this, 'load_upcoming_events_shortcode' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'load_shortcode_scripts' ) );
 	}
 
@@ -47,7 +47,7 @@ class Cpsc_Frontend {
 	 * @param array $atts Shortcode attributes.
 	 * @return string
 	 */
-	public function load_workshop_shortcode( $atts = array() ) {
+	public function load_upcoming_events_shortcode( $atts = array() ) {
 
 		if ( is_admin() ) {
 			return;
@@ -62,9 +62,30 @@ class Cpsc_Frontend {
 			'workshops'
 		);
 
-		$type = strtolower( trim( $atts['type'] ) );
-		$show_date = wp_unslash( boolval( $atts['show_date'] ) );
+		// Fetch workshops based on the type
+		$workshops = $this->fetch_upcoming_workshops( $atts );
+		
+		// Fetch courses for all types
+		$courses = $this->fetch_tutorlms_courses();
+		
+		// Combine workshops and courses
+		$all_items = array_merge( $workshops, $courses );
+		
+		// Display combined items
+		return $this->display_combined_items( $all_items, $atts );
+	}
 
+	/**
+	 * Fetch workshops based on type
+	 *
+	 * @since 1.0.0
+	 * @param array $atts Shortcode attributes.
+	 * @return array
+	 */
+	private function fetch_upcoming_workshops( $atts = array() ) {
+		$show_date = wp_unslash( boolval( $atts['show_date'] ) );
+		$type = strtolower( trim( $atts['type'] ) );
+		
 		$today = date( 'Y-m-d' );
 		$tomorrow = date( 'Y-m-d', strtotime( '+1 day' ) );
 
@@ -120,36 +141,107 @@ class Cpsc_Frontend {
 			$args['meta_query'] = $meta_query;
 		}
 
-		$result = '';
+		$workshops = array();
 		$query = new WP_Query( $args );
 
 		if ( $query->have_posts() ) :
-			$result .= '<ul class="sas-workshops columns-3">';
 			while ( $query->have_posts() ) :
 				$query->the_post();
 
 				$post_image = get_the_post_thumbnail( null, array(300,300), '' ) ? get_the_post_thumbnail( null, array(300,300), '' ) : '<img src="' . CPSC_URL . "assets/images/400x400.jpg" . '">';
-				$result .=  '<li class="sas-workshop type-workshop">';
-				$result .=  '<div class="sas-workshop-thumbnail-wrap">';
-				$result .=  '<a href="' . get_the_permalink() . '" class="sas-workshop-link">' . $post_image . '</a>';
-				$result .=  '</div>';
-				$result .=  '<div class="sas-workshop-summary-wrap">';
-				$result .=  '<span class="sas-workshop-category">Workshop</span> ';
-				if( $show_date ){
-					$start_date = get_post_meta( get_the_ID(), '_cpsc_workshop_start_date', true );
-					$formatted_date = date('d-M-Y', strtotime($start_date));
-
-					$result .=  '<span class="sas-workshop__event-date"><span class="dashicons dashicons-calendar-alt"></span>' . $formatted_date . '</span>';
-				}
-				$result .=  '<a href="' . get_the_permalink() . '" class="sas-workshop__link">';
-				$result .=  '<h2 class="sas-workshop__title">' . get_the_title() . '</h2>';
-				$result .=  '</a>';
-				$result .=  '<a href="' . get_the_permalink() . '" class="sas-workshop__button">Read More</a>';
-				$result .=  '</div>';
-				$result .=  '</li>';
+				
+				$workshop_data = array(
+					'type' => 'workshop',
+					'image' => $post_image,
+					'permalink' => get_the_permalink(),
+					'title' => get_the_title(),
+					'category' => 'Workshop',
+					'date' => $show_date ? get_post_meta( get_the_ID(), '_cpsc_workshop_start_date', true ) : '',
+				);
+				
+				$workshops[] = $workshop_data;
 			endwhile;
-			$result .= '</ul>';
 			wp_reset_postdata();
+		endif;
+
+		return $workshops;
+	}
+
+	/**
+	 * Fetch published TutorLMS courses
+	 *
+	 * @since 1.0.0
+	 * @return array
+	 */
+	private function fetch_tutorlms_courses() {
+		$courses = array();
+		
+		$args = array(
+			'post_type'      => 'courses',
+			'posts_per_page' => '-1',
+			'post_status'    => 'publish',
+		);
+
+		$query = new WP_Query( $args );
+
+		if ( $query->have_posts() ) :
+			while ( $query->have_posts() ) :
+				$query->the_post();
+				
+				$post_image = get_the_post_thumbnail( null, array(300,300), '' ) ? get_the_post_thumbnail( null, array(300,300), '' ) : '<img src="' . CPSC_URL . "assets/images/400x400.jpg" . '">';
+				
+				$course_data = array(
+					'type' => 'course',
+					'image' => $post_image,
+					'permalink' => get_the_permalink(),
+					'title' => get_the_title(),
+					'category' => 'Course',
+					'date' => '',
+				);
+				
+				$courses[] = $course_data;
+			endwhile;
+			wp_reset_postdata();
+		endif;
+
+		return $courses;
+	}
+
+	/**
+	 * Display combined workshops and courses
+	 *
+	 * @since 1.0.0
+	 * @param array $items Array of workshops and courses.
+	 * @param array $atts Shortcode attributes.
+	 * @return string
+	 */
+	private function display_combined_items( $items, $atts = array() ) {
+		$show_date = wp_unslash( boolval( $atts['show_date'] ) );
+		$result = '';
+		
+		if ( ! empty( $items ) ) :
+			$result .= '<ul class="sas-workshops columns-3">';
+			foreach ( $items as $item ) :
+				$result .= '<li class="sas-workshop type-' . $item['type'] . '">';
+				$result .= '<div class="sas-workshop-thumbnail-wrap">';
+				$result .= '<a href="' . $item['permalink'] . '" class="sas-workshop-link">' . $item['image'] . '</a>';
+				$result .= '</div>';
+				$result .= '<div class="sas-workshop-summary-wrap">';
+				$result .= '<span class="sas-workshop-category">' . $item['category'] . '</span> ';
+				
+				if ( $show_date && ! empty( $item['date'] ) ) :
+					$formatted_date = date('d-M-Y', strtotime($item['date']));
+					$result .= '<span class="sas-workshop__event-date"><span class="dashicons dashicons-calendar-alt"></span>' . $formatted_date . '</span>';
+				endif;
+				
+				$result .= '<a href="' . $item['permalink'] . '" class="sas-workshop__link">';
+				$result .= '<h2 class="sas-workshop__title">' . $item['title'] . '</h2>';
+				$result .= '</a>';
+				$result .= '<a href="' . $item['permalink'] . '" class="sas-workshop__button">Read More</a>';
+				$result .= '</div>';
+				$result .= '</li>';
+			endforeach;
+			$result .= '</ul>';
 		endif;
 
 		return $result;
